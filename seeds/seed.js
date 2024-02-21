18f6c2aec01e96b5ebbfb9844bfeabef2bca6840
benitaisaac <benita.isaac@gmail.com>
2024-02-21 15:28:01 -0800

    created seedAll function

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,3 +1,22 @@
 const sequelize = require('../config/connection');
-const { User, Comment, Blogpost} = require('../models');
+
+//import all seeding functions
+const seedUsers = require('./userData');
+const seedBlogPost = require('./blogPostData');
+const seedComments = require('./commentData');;
+
+//define a new function to seed all data
+const seedAll = async () => {
+    await sequelize.sync({force: true});
+
+    await seedUsers();
+
+    await seedComments();
+
+    await seedBlogPost();
+
+    process.exit(0);
+};
+
+seedAll();
 
