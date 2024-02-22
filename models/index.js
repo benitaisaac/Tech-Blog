8abc3db1845f75ce4bf93b0760de6faed4513110
benitaisaac <benita.isaac@gmail.com>
2024-02-21 16:37:41 -0800

    changed associations between models in index.js file

--- models/index.js
+++ models/index.js
@@ -1,33 +1,21 @@
-//import all models that we will work with 
-const User = require('./User');
-const Blogpost = require ('./blogPost');
-const Comment = require('./Comment');
+//import all models that we will work with
+const User = require("./User");
+const Blogpost = require("./blogPost");
+const Comment = require("./Comment");
 
-// .belongsTo associations -- 
-Blogpost.belongsTo(User);
-
-Comment.belongsTo(Blogpost);
-
-Comment.belongsTo(User);
-
-// .hasMany associations -- 
-
-User.hasMany(Blogpost, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
-
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    //TODO: does this need a delete option? 
+Blogpost.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 Blogpost.hasMany(Comment, {
-    foreignKey: 'blogpost_id',
-    //TODO: delete option? 
-})
+  foreignKey: "blogpost_id",
+  onDelete: "CASCADE",
+});
 
-module.exports = { Blogpost, User, Comment};
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
-
-
+module.exports = { Blogpost, User, Comment };
