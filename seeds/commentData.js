2ff2296d77910dda3ad92c3cf416c0aa6c24fa49
benitaisaac <benita.isaac@gmail.com>
2024-02-23 18:00:01 -0800

    fixed bug with comment model

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -1,9 +1,9 @@
 const {Comment} = require('../models');
 const commentData = [
-    {content: "sample content 1", user_id: 1, blogPost_id: 1},
-    {content: "sample content 2", user_id: 2, blogPost_id: 2},
-    {content: "sample content 3", user_id: 3, blogPost_id: 3},
-    {content: "sample content 4", user_id: 4, blogPost_id: 4},
+    {content: "sample content 1", user_id: 1, blogpost_id: 1},
+    {content: "sample content 2", user_id: 2, blogpost_id: 2},
+    {content: "sample content 3", user_id: 3, blogpost_id: 3},
+    {content: "sample content 4", user_id: 4, blogpost_id: 4},
 ];
 
 const seedComments = () => Comment.bulkCreate(commentData);
