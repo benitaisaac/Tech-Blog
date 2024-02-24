feae2272c4c1de39e35527d7346973e5e4da416c
benitaisaac <benita.isaac@gmail.com>
2024-02-24 10:33:03 -0800

    added login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-// create a user 
+// create a new user 
 router.post("/", async(req, res) => {
     try{
         const newUser = await User.create(req.body)
@@ -17,7 +17,35 @@ router.post("/", async(req, res) => {
 });
 
 // create a login route
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({where: { userName: req.body.userName}});
+
+        if (!userData) {
+            res.status(400).json({ message: "Incorrect email or password, please try again"});
+            return;
+        }
+
+        const validPassword = await User.findOne({where: {password: req.body.password}});
+
+        if(!validPassword) {
+            res.status(400).json({message: "Incorrect email or password, please try again"});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({user: userData, message: 'You are logged in!'});
+        });
+        
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 // create a logout route 
+
 
 module.exports = router;
