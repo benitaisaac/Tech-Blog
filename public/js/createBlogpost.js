c21e3f495f74b6973ef7faabdb13da2bd5404369
benitaisaac <benita.isaac@gmail.com>
2024-03-02 12:54:58 -0800

    updated id for updating a post partial

--- public/js/createBlogpost.js
+++ public/js/createBlogpost.js
@@ -1,6 +1,13 @@
+//When you click create blogpost, the form appears and the button disappears 
 document.getElementById('create-blogpost-btn').addEventListener('click', function() {
     document.getElementById('create-blogpost-btn').style.display = 'none';
     document.getElementById('create-blogpost-form').style.display= 'block';
+});
+
+//When you update a blogpost, the update form appears and the button disappears
+document.getElementById('update-post-btn').addEventListener('click', function() {
+    document.getElementById('update-post-btn').style.display = 'none';
+    document.getElementById('update-blogpost-form').style.display= 'block';
 });
 
 
