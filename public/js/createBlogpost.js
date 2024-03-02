c9595d1325f2f0009b8301efd3f3dc655bcc435b
benitaisaac <benita.isaac@gmail.com>
2024-03-01 22:05:33 -0800

    user is able to add blogposts now

---
+++ public/js/createBlogpost.js
@@ -0,0 +1,28 @@
+document.getElementById('create-blogpost-btn').addEventListener('click', function() {
+    document.getElementById('create-blogpost-btn').style.display = 'none';
+    document.getElementById('create-blogpost-form').style.display= 'block';
+});
+
+
+const createBlogFormHandler = async (event) => {
+    event.preventDefault();
+//collect values from the form 
+const title = document.querySelector("#blogpost-title").value.trim();
+const content = document.querySelector("#blogpost-content").value.trim();
+
+if (title && content) {
+    const response = await fetch("/api/blogPosts", {
+        method: "POST",
+        body: JSON.stringify({ title, content}),
+        headers: {"Content-Type": "application/json"},
+    });
+
+    if (response.ok) {
+        document.location.replace("/profile");
+    } else {
+        alert(response.statusText);
+    }
+}
+};
+
+document.querySelector('.create-blogpost-form').addEventListener('submit', createBlogFormHandler);
