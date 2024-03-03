a4d811cfd6bb68ffc9c0233d1f7b1a1ee4094130
benitaisaac <benita.isaac@gmail.com>
2024-03-02 21:49:08 -0800

    added button handler function in order to update info

--- public/js/userblogs.js
+++ public/js/userblogs.js
@@ -1,3 +1,15 @@
+//This form will handle updating and deleting blogposts
+// since these are the two buttons on each div that displays a user's blogposts
+
+const updateBlogpostButton = document.getElementById('update-post-btn');
+const updatedBlogpostContent = document.getElementById('update-blogpost-content');
+
+//When you update a blogpost, the update form appears and the button disappears
+updateBlogpostButton.addEventListener('click', function() {
+    document.getElementById('update-post-btn').style.display = 'none';
+    document.getElementById('update-blogpost-form').style.display= 'block';
+});
+
 //create delete button form handler
 const delButtonHandler = async(event) => {
     if (event.target.hasAttribute('data-id')) {
@@ -15,7 +27,33 @@ const delButtonHandler = async(event) => {
 };
 
 //create update button form handler 
+const updateButtonHandler = async(event) => {
+    event.preventDefault();
+    //collect updated values from the form
+    const title = document.querySelector('#update-blogpost-title').value.trim();
+    const content = document.querySelector('#update-blogpost-content').value.trim();
+    //collect id data to add in route fetch request
+    const id = event.target.getAttribute('data-id');
+
+// Create fetch request, sending over all data
+    if(title && content) {
+        const response = await fetch(`api/blogPosts/${id}`, {
+            method: "PUT",
+            body: JSON.stringify({ title, content}),
+            headers: {"Content-Type": "application/json"},
+        });
+
+        if (response.ok){
+            document.location.replace("/profile");
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
 
 //call delete button form handler 
 document.querySelector('#delete-post-btn').addEventListener('click', delButtonHandler);
+
+document.querySelector('#submit-update-blogpost').addEventListener('click', updateButtonHandler);
 
