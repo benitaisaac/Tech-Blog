6c99600cc8beb5a7383e71bc156b270a3c83d91f
benitaisaac <benita.isaac@gmail.com>
2024-03-06 19:12:29 -0800

    troubleshooting update button

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,14 @@
 const createBlogpostButton = document.getElementById('create-blogpost-btn');
 
+const updateBlogpostButton = document.getElementById('update-post-btn');
+
+updateBlogpostButton.addEventListener('click', function() {
+    document.getElementById('update-post-btn').style.display = 'none';
+    document.getElementById('update-blogpost-form').style.display= 'block';
+});
+
+
+
 //When you click create blogpost, the form appears and the button disappears 
 createBlogpostButton.addEventListener('click', function() {
     document.getElementById('create-blogpost-btn').style.display = 'none';
@@ -27,5 +36,52 @@ if (title && content) {
     }
 }
 };
+//create delete button form handler
+const delButtonHandler = async(event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+        const response = await fetch(`/api/blogPosts/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to delete project');
+        }
+    }
+};
+
+//create update button form handler 
+const updateButtonHandler = async(event) => {
+    event.preventDefault();
+    //collect updated values from the form
+    const title = document.querySelector('#update-blogpost-title').value.trim();
+    const content = document.querySelector('#update-blogpost-content').value.trim();
+    //collect id data to add in route fetch request
+    const id = event.target.getAttribute('data-id');
+
+// Create fetch request, sending over all data
+    if(title && content) {
+        const response = await fetch(`api/blogPosts/${id}`, {
+            method: "PUT",
+            body: JSON.stringify({ title, content}),
+            headers: {"Content-Type": "application/json"},
+        });
+
+        if (response.ok){
+            document.location.replace("/profile");
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+
+//call delete button form handler 
+document.querySelector('#delete-post-btn').addEventListener('click', delButtonHandler);
+
+document.querySelector('#submit-update-blogpost').addEventListener('click', updateButtonHandler);
+
 
 document.querySelector('.create-blogpost-form').addEventListener('submit', createBlogFormHandler);
