f46eb96e782b780b3bf2df6d59961c33e450f88e
benitaisaac <benita.isaac@gmail.com>
2024-03-09 11:30:26 -0800

    frontend for adding comments finished

--- public/js/blogpost.js
+++ public/js/blogpost.js
@@ -6,16 +6,18 @@ const commentHandler = async (event) => {
 
   const content = document.querySelector("#comment").value.trim();
   //TODO: get the id of the blogpost so we can render the correct page when data is sent 
+  const blogpost_id = event.target.dataset.id;
 
   if (content) {
     const response = await fetch("/api/comment", {
       method: "POST",
-      body: JSON.stringify({ content }),
+      body: JSON.stringify({ content, blogpost_id }),
       headers: { "content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace("/blogPost/{{id}}");
+        console.log(response);
+      document.location.replace(`/blogPost/${blogpost_id}`);
     } else {
       alert(response.statusText);
     }
