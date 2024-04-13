f55831c6f2c63fff13d70be30fb8a5f62e2a998f
Benita <benita.isaac@gmail.com>
2024-04-12 18:11:17 -0700

    added user data to post route for comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,14 +2,12 @@ const router = require('express').Router();
 const {Comment} = require ('../../models');
 
 // Route for creating comments 
-//TODO: add withAuth middleware? 
-//TODO: make sure it's connected to a blog post?
 router.post('/', async (req, res) => {
     try {
-        const newComment = await Comment.create(
-            req.body
-            // user_id: req.session.user_id,
-        );
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
         res.status(200).json(newComment);
     } catch(err) {
         res.status(400).json(err);
